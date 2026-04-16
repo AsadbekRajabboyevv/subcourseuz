ac8dc569601fde95db682b4cd4899f1c32485bfe
Asadbek Rajabboyev <rajabboyev.asadbek@uzinfocom.uz>
2026-04-16 17:40:44 +0500

    FE fix: payment modal changed

--- src/main/webapp/app/features/payment/modal/payment-modal.component.ts
+++ src/main/webapp/app/features/payment/modal/payment-modal.component.ts
@@ -1,43 +1,29 @@
-import { Component, EventEmitter, Input, Output, inject, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {InputComponent} from "../../../shared/ui/forms/input.component";
-import {PaymentService} from "../payment.service";
-import {PaymentRequestDto} from "../payment.model";
-
 @Component({
   selector: 'app-payment-modal',
   templateUrl: './payment-modal.component.html',
   standalone: true,
   imports: [CommonModule, FormsModule, InputComponent]
 })
+
 export class PaymentModalComponent {
-  private paymentService = inject(PaymentService);
-
-  @Input() itemData: any;
+  @Input({ required: true }) item: any;
   @Input() type: 'COURSE' | 'TEST' = 'COURSE';
+
   @Output() close = new EventEmitter<void>();
-  @Output() success = new EventEmitter<any>();
+  @Output() confirm = new EventEmitter<{ couponCode: string }>();
 
-  couponCode = signal('');
-  isLoading = signal(false);
+  couponCode = '';
 
-  confirmPurchase() {
-    this.isLoading.set(true);
-
-    const request: PaymentRequestDto = {
-      courseId: this.type === 'COURSE' ? this.itemData.id : null,
-      testId: this.type === 'TEST' ? this.itemData.id : null,
-      amount: this.itemData.price,
-      couponCode: this.couponCode()
-    };
-
-    this.paymentService.purchase(request).subscribe({
-      next: (res) => {
-        this.isLoading.set(false);
-        this.success.emit(res.data);
-      },
-      error: () => this.isLoading.set(false)
-    });
+  onConfirm() {
+    this.confirm.emit({ couponCode: this.couponCode });
   }
+
+  onClose() {
+    this.close.emit();
+  }
+
 }
