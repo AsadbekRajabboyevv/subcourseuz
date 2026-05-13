51a739d0d2fc7f4afd251611037e28019f5e1def
Asadbek Rajabboyev <rajabboyev.asadbek@uzinfocom.uz>
2026-05-13 16:43:46 +0500

    Remove unused `ObjectMapper` import from `DomainConfig`

--- src/main/java/uz/asadbek/subcourse/config/DomainConfig.java
+++ src/main/java/uz/asadbek/subcourse/config/DomainConfig.java
@@ -1,6 +1,5 @@
 package uz.asadbek.subcourse.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
